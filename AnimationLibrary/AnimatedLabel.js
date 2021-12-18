f638e591ca0da4f4553b3e2efd10476087447527
Tuấn Huỳnh <huynhtuan0701@gmail.com>
2021-12-18 23:54:54 +0700

    3 file .html + update library

--- AnimationLibrary/AnimatedLabel.js
+++ AnimationLibrary/AnimatedLabel.js
@@ -39,7 +39,7 @@ AnimatedLabel.prototype.draw = function(ctx)
 	}
 	
 	ctx.globalAlpha = this.alpha;
-	ctx.font = '10px sans-serif';
+	ctx.font = '20px sans-serif';
 
 	var startingXForHighlight = this.x; 
 
